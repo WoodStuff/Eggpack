55876c570f865254cbc1fa8a66c06e575c2e0801
WoodStuff <woodstuffthings@gmail.com>
2022-06-07 21:20:35 +0200

    that was uneven

--- Elements/Accessories/HellstoneShield.cs
+++ Elements/Accessories/HellstoneShield.cs
@@ -19,7 +19,7 @@ namespace eggpack.Elements.Accessories
 		{
 			Item.width = 32;
 			Item.height = 32;
-			Item.value = 350000;
+			Item.value = 375000;
 			Item.rare = ItemRarityID.Blue;
 			Item.accessory = true;
 			Item.defense = 5;
