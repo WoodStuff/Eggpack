90abfb76c70783abec49b5e7ca7a9af67c51b12e
WoodStuff <woodstuffthings@gmail.com>
2022-06-13 21:36:03 +0200

    metal orb

---
+++ Elements/Weapons/Summons/SMetalOrb/MetalOrbBuff.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace eggpack.Elements.Weapons.Summons.SMetalOrb
+{
+	public class MetalOrbBuff : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Metal Orb");
+			Description.SetDefault("The metal orb will fight for you");
+
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<MetalOrb>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
---
+++ Elements/Weapons/Summons/SMetalOrb/MetalStaff.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace eggpack.Elements.Weapons.Summons.SMetalOrb
+{
+	/*
+	 * This file contains all the code necessary for a minion
+	 * - ModItem
+	 *     the weapon which you use to summon the minion with
+	 * - ModBuff
+	 *     the icon you can click on to despawn the minion
+	 * - ModProjectile 
+	 *     the minion itself
+	 *     
+	 * It is not recommended to put all these classes in the same file. For demonstrations sake they are all compacted together so you get a better overwiew.
+	 * To get a better understanding of how everything works together, and how to code minion AI, read the guide: https://github.com/tModLoader/tModLoader/wiki/Basic-Minion-Guide
+	 * This is NOT an in-depth guide to advanced minion AI
+	 */
+
+	public class MetalStaff : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Summons a metal orb to fight for you");
+
+			ItemID.Sets.GamepadWholeScreenUseRange[Type] = true; // This lets the player target anywhere on the whole screen while using a controller.
+			ItemID.Sets.LockOnIgnoresCollision[Type] = true;
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 5;
+			Item.knockBack = 3f;
+			Item.mana = 10;
+			Item.width = 64;
+			Item.height = 64;
+			Item.useTime = 36;
+			Item.useAnimation = 36;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.value = Item.sellPrice(0, 30, 0, 0);
+			Item.rare = ItemRarityID.White;
+			Item.UseSound = SoundID.Item44;
+
+			// These below are needed for a minion weapon
+			Item.noMelee = true;
+			Item.DamageType = DamageClass.Summon;
+			Item.buffType = ModContent.BuffType<MetalOrbBuff>();
+			// No buffTime because otherwise the item tooltip would say something like "1 minute duration"
+			Item.shoot = ModContent.ProjectileType<MetalOrb>();
+		}
+
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			// Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position
+			position = Main.MouseWorld;
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			// This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+			player.AddBuff(Item.buffType, 2);
+
+			// Minions have to be spawned manually, then have originalDamage assigned to the damage of the summon item
+			var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+			projectile.originalDamage = Item.damage;
+
+			// Since we spawned the projectile manually already, we do not need the game to spawn it for ourselves anymore, so return false
+			return false;
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddRecipeGroup(RecipeGroupID.IronBar, 12)
+				.AddTile(TileID.Anvils)
+				.Register();
+		}
+	}
+}
