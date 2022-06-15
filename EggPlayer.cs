8aa3fd5892cdf36e93617171f76b8c6f5faecf89
WoodStuff <woodstuffthings@gmail.com>
2022-06-15 18:17:49 +0200

    g

--- EggPlayer.cs
+++ EggPlayer.cs
@@ -2,20 +2,21 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.Localization;
 using Terraria.ID;
+using System.Collections.Generic;
 
 namespace eggpack
 {
-    public class EggPlayer : ModPlayer
-    {
-        public bool hasHellSharpener;
+	public class EggPlayer : ModPlayer
+	{
+		public bool hasHellSharpener;
 
 		public override void ResetEffects()
 		{
 			hasHellSharpener = false;
 		}
 		public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
-        {
-            if (hasHellSharpener) target.AddBuff(BuffID.OnFire, 180);
-        }
+		{
+			if (hasHellSharpener) target.AddBuff(BuffID.OnFire, 180);
+		}
     }
 }
