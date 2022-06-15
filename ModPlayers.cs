0ff955ad0e4a2428af78617bb03ce4b59efca965
WoodStuff <woodstuffthings@gmail.com>
2022-06-15 16:06:02 +0200

    g

--- ModPlayers.cs
+++ ModPlayers.cs
@@ -7,9 +7,9 @@ namespace eggpack
 {
     public class ModPlayers : ModPlayer
     {
-        public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
+        /*public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
         {
             target.AddBuff(BuffID.OnFire, 180);
-        }
+        }*/
     }
 }
