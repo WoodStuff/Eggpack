81b1c43665c1471f70af61f2393f58af171c06eb
WoodStuff <woodstuffthings@gmail.com>
2022-07-16 12:31:06 +0200

    readability

--- Elements/Projectiles/ThingiteBurst.cs
+++ Elements/Projectiles/ThingiteBurst.cs
@@ -76,14 +76,16 @@ namespace eggpack.Elements.Projectiles
 			int particles = 16;
 			for (int i = 0; i < particles; i++)
 			{
-				int particle = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y),
+				int particle = Dust.NewDust(
+					new Vector2(Projectile.position.X, Projectile.position.Y),
 					Projectile.width,
 					Projectile.height,
 					DustID.RainbowTorch,
 					new Vector2(0, 1).RotatedBy(Math.PI * 2 / particles).X * 3,
 					new Vector2(0, 1).RotatedBy(Math.PI * 2 / particles).Y * 3,
 					100,
-					new Color(192, 192, 192));
+					new Color(192, 192, 192)
+				);
 				Main.dust[particle].noGravity = true;
 			}
 		}
