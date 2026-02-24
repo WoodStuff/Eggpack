b23a83ea513ef0ab5a2bc75faf74c9eb09cea918
TheEgglet <woodstuffthings@gmail.com>
2026-02-24 18:38:20 +0100

    clean up sword files

--- Elements/Weapons/Melee/Swords/GraniteSword.cs
+++ Elements/Weapons/Melee/Swords/GraniteSword.cs
@@ -15,21 +15,32 @@ public class GraniteSword : ModItem
 	{
 		Item.ResearchUnlockCount = 1;
 	}
+
 	public override void SetDefaults()
 	{
 		Item.CloneDefaults(ItemID.IceBlade);
-		Item.SetWeaponValues(18, 7, 3);
-		Item.SetShopValues(ItemRarityColor.Green2, Item.sellPrice(0, 1, 0, 0));
-		Item.DamageType = DamageClass.Melee;
-		Item.useTime = 48;
-		Item.useAnimation = 24;
-		Item.shoot = ModContent.ProjectileType<GraniteSwordProjectile>();
-		Item.shootSpeed = 9f;
-		Item.scale = 1.1f;
 
-		Item.width = 40;
-		Item.height = 40;
+		// sword properties
+		Item.DamageType = DamageClass.Melee;
 		Item.useStyle = ItemUseStyleID.Swing;
 		Item.UseSound = SoundID.Item1;
+
+		// sword stats
+		Item.damage = 18;
+		Item.knockBack = 7;
+		Item.useTime = 48;
+		Item.useAnimation = 24;
+		Item.crit = 3;
+		Item.scale = 1.1f;
+
+		// projectile stats
+		Item.shoot = ModContent.ProjectileType<GraniteSwordProjectile>();
+		Item.shootSpeed = 9f;
+
+		// item properties
+		Item.width = 40;
+		Item.height = 40;
+		Item.value = Item.sellPrice(0, 1, 0, 0);
+		Item.rare = ItemRarityID.Green;
 	}
 }
--- Elements/Weapons/Melee/Swords/ThingiteSword.cs
+++ Elements/Weapons/Melee/Swords/ThingiteSword.cs
@@ -14,20 +14,26 @@ public class ThingiteSword : ModItem
 	{
 		Item.ResearchUnlockCount = 1;
 	}
+
 	public override void SetDefaults()
 	{
-		Item.damage = 17;
+		// sword properties
 		Item.DamageType = DamageClass.Melee;
-		Item.width = 32;
-		Item.height = 32;
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.UseSound = SoundID.Item1;
+
+		// sword stats
+		Item.damage = 17;
+		Item.knockBack = 6;
 		Item.useTime = 21;
 		Item.useAnimation = 21;
-		Item.useStyle = ItemUseStyleID.Swing;
-		Item.knockBack = 6;
+		Item.scale = 1.2f;
+
+		// item properties
+		Item.width = 32;
+		Item.height = 32;
 		Item.value = Item.sellPrice(0, 0, 30, 0);
 		Item.rare = ItemRarityID.Blue;
-		Item.UseSound = SoundID.Item1;
-		Item.scale = 1.2f;
 	}
 
 	public override void AddRecipes()
@@ -40,7 +46,6 @@ public class ThingiteSword : ModItem
 
 	public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
 	{
-		if (!hit.Crit) return;
-		target.AddBuff(BuffID.Weak, 3 * 60);
+		if (hit.Crit) target.AddBuff(BuffID.Weak, 3 * 60);
 	}
 }
